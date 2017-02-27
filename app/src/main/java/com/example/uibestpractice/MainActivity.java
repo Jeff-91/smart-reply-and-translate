2e7e2650d21e35e1acf9d5041ad4dd0c5d5a00c9
Jeff-91 <huangjunfeng91@outlook.com>
2017-02-27 10:29:43 +0800

    delete some useless code,and add one more little game.

--- app/src/main/java/com/example/uibestpractice/MainActivity.java
+++ app/src/main/java/com/example/uibestpractice/MainActivity.java
@@ -6,33 +6,18 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-
-import static android.R.id.list;
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
-import static android.os.Build.VERSION_CODES.M;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,9 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static boolean isEnglish = false;
 
-    private static boolean isSpace = false;
+    private static boolean once = false;
+    private static boolean twice = false;
+    private static boolean thirdtime = false;
 
-    private static boolean isAgain = false;
 
 
     @Override
@@ -70,81 +56,73 @@ public class MainActivity extends AppCompatActivity {
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String content = inputText.getText().toString();
-//                boolean isEnglish = false;
                 for (int i = 0; i < content.length();i++)
                 {
                     int num = content.charAt(i);
                     if((num>='a'&&num<='z')||(num>='A'&&num<='Z'))
                     {
                         isEnglish = true;
-//                        break;
                     }
                 }
-                if (!content.isEmpty()){
-                    if (content.charAt(0) == ' ') {
-                        isEnglish = false;
-                        Msg msg = new Msg(content, Msg.TYPE_SENT);
-                        msgList.add(msg);
-                        sendRequestWithOkHttp(content);
-                        Log.d("something", "==================00000=========");
-                        adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
-                        msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
-                        inputText.setText(""); // 清空输入框中的内容
-                    } else {
-                        if (isEnglish) {
+                if ("I love you".equals(content)) {
+                    isEnglish = false;
+                    ++add3;
+                    if (add3 == 1) {
+                        once = true;
+                        twice = false;
+                        thirdtime = false;
+                    } else if (add3 == 2) {
+                        once = false;
+                        twice = true;
+                        thirdtime = false;
+                    } else if (add3 == 3) {
+                        once = false;
+                        twice = false;
+                        thirdtime = true;
+                        add3 = 0;
+                    }
+                    }
+
+                if (once || twice || thirdtime) {
+                    Msg msg = new Msg(content, Msg.TYPE_SENT);
+                    msgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
+                    inputText.setText(""); // 清空输入框中的内容
+                    showResponse1("win");
+
+                } else {
+                    add3 = 0;
+                    if (!content.isEmpty()){
+                        if (content.charAt(0) == ' ') {
+                            isEnglish = false;
                             Msg msg = new Msg(content, Msg.TYPE_SENT);
                             msgList.add(msg);
                             sendRequestWithOkHttp(content);
-                            Log.d("something", "==================00000=========");
                             adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
                             msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
                             inputText.setText(""); // 清空输入框中的内容
                         } else {
-                            Msg msg = new Msg(content, Msg.TYPE_SENT);
-                            msgList.add(msg);
-                            sendRequestWithOkHttp1(content);
-                            Log.d("something", "==================00000=========");
-                            adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
-                            msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
-                            inputText.setText(""); // 清空输入框中的内容
+                            if (isEnglish) {
+                                Msg msg = new Msg(content, Msg.TYPE_SENT);
+                                msgList.add(msg);
+                                sendRequestWithOkHttp(content);
+                                adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
+                                msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
+                                inputText.setText(""); // 清空输入框中的内容
+                            } else {
+                                Msg msg = new Msg(content, Msg.TYPE_SENT);
+                                msgList.add(msg);
+                                sendRequestWithOkHttp1(content);
+                                adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
+                                msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
+                                inputText.setText(""); // 清空输入框中的内容
+                            }
                         }
-
                     }
-                }
-//                int k = content.charAt(0);
-//                if (k == ' ' && !content.isEmpty()){
-//                    isSpace = true;
-//                    isEnglish = false;
-//                }
-//                if (isSpace && !content.isEmpty()) {
-//                    Msg msg = new Msg(content, Msg.TYPE_SENT);
-//                    msgList.add(msg);
-//                    sendRequestWithOkHttp(content);
-//                    adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
-//                    msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
-//                    inputText.setText(""); // 清空输入框中的内容
-//                    isSpace = false;
-//                } else if (!content.isEmpty()) {
-//                    if ( isEnglish) {
-//                        Msg msg = new Msg(content, Msg.TYPE_SENT);
-//                        msgList.add(msg);
-//                        sendRequestWithOkHttp(content);
-//                        adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
-//                        msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
-//                        inputText.setText(""); // 清空输入框中的内容
-//                    } else  {
-//                        Msg msg = new Msg(content, Msg.TYPE_SENT);
-//                        msgList.add(msg);
-//                        sendRequestWithOkHttp1(content);
-//                        adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
-//                        msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
-//                        inputText.setText(""); // 清空输入框中的内容
-//                    }
-//
-//                }
 
+                }
             }
         });
 
@@ -163,8 +141,6 @@ public class MainActivity extends AppCompatActivity {
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
                     parseJSONWithJSONObject(responseData);
-                    Log.d("something", "==================00001=========");
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -184,7 +160,6 @@ public class MainActivity extends AppCompatActivity {
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
                     parseJSONWithJSONObject1(responseData);
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -198,7 +173,6 @@ public class MainActivity extends AppCompatActivity {
             String text = jsonObject.getString("translation");
             String text1 = text.substring(2,text.length()-2);
             showResponse(text1);
-            Log.d("something", "==================00002=========");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -218,7 +192,6 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(MainActivity.this,"This is english",Toast.LENGTH_SHORT).show();
                 if (isEnglish) {
                     sendRequestWithOkHttp1(response);
                 } else {
@@ -236,17 +209,38 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(MainActivity.this,"这是中文",Toast.LENGTH_SHORT).show();
-                if (isEnglish) {
-                    sendRequestWithOkHttp(response);
-                    isEnglish = false;
-                } else {
-                    Msg msg = new Msg(response, Msg.TYPE_RECEIVED);
+                if (once) {
+                    String response1 = "I don't know what are you talking about";
+                    Msg msg = new Msg(response1, Msg.TYPE_RECEIVED);
                     msgList.add(msg);
                     adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
                     msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
+                    once = false;
+                } else if (twice){
+                    String response1 = "Are you sure?";
+                    Msg msg = new Msg(response1, Msg.TYPE_RECEIVED);
+                    msgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
+                    twice = false;
+                } else if (thirdtime) {
+                    String response1 = "哈哈哈，恭喜获得彩蛋!I love you too,mua!";
+                    Msg msg = new Msg(response1, Msg.TYPE_RECEIVED);
+                    msgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
+                    thirdtime = false;
+                } else {
+                    if (isEnglish) {
+                        sendRequestWithOkHttp(response);
+                        isEnglish = false;
+                    } else {
+                        Msg msg = new Msg(response, Msg.TYPE_RECEIVED);
+                        msgList.add(msg);
+                        adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
+                        msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
+                    }
                 }
-
             }
         });
     }
@@ -254,10 +248,8 @@ public class MainActivity extends AppCompatActivity {
     private void initMsgs() {
         Msg msg1 = new Msg("hello，无聊的话我可以陪你聊天哦", Msg.TYPE_RECEIVED);
         msgList.add(msg1);
-        Msg msg2 = new Msg("发英文的话我可以给你翻译", Msg.TYPE_RECEIVED);
+        Msg msg2 = new Msg("第一个字前加空格我可以给你翻译哦", Msg.TYPE_RECEIVED);
         msgList.add(msg2);
-        Msg msg3 = new Msg("翻译中文的话在第一个字前面加个空格就好了", Msg.TYPE_RECEIVED);
-        msgList.add(msg3);
     }
 
 }
